8adb637c5869fa870b4b23a7d7f401391cc774e1
PasupulaNarasimha <137201913+PasupulaNarasimha@users.noreply.github.com>
2026-01-14 23:05:43 +0530

    Update ApplicationController.java

--- EduTech/src/main/java/com/edutech/contorller/ApplicationController.java
+++ EduTech/src/main/java/com/edutech/contorller/ApplicationController.java
@@ -41,10 +41,10 @@ public class ApplicationController {
 	public @ResponseBody ResponseEntity<?> display(@RequestBody Login login) {
 		User user = service.findByEmail(login.getEmailid());
 		if(user==null) {
-			return new ResponseEntity<>("Enter A valid EmailId", HttpStatus.BAD_REQUEST);
+			return new ResponseEntity<>("Correct the emailId", HttpStatus.BAD_REQUEST);
 		}
 		if(!user.getPassword().equals(login.getPassword())) {
-			return new ResponseEntity<>("Enter A valid password", HttpStatus.BAD_REQUEST);
+			return new ResponseEntity<>("Correct the password", HttpStatus.BAD_REQUEST);
 		}
 		return new ResponseEntity<>("Login Successfully", HttpStatus.OK);
 	}
